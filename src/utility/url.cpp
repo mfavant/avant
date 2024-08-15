7e98bf4fb51b28666ba412898d8e551c2e190a89
gaowanlu <2209120827@qq.com>
2024-08-15 20:11:54 +0800

    feat: utility url plan

--- src/utility/url.cpp
+++ src/utility/url.cpp
@@ -4,6 +4,128 @@
 
 using avant::utility::url;
 
+url::url()
+{
+}
+
+url::url(const std::string &s)
+{
+}
+
+std::string url::get_scheme() const
+{
+    return "";
+}
+
+std::string url::get_username() const
+{
+    return "";
+}
+
+std::string url::get_password() const
+{
+    return "";
+}
+
+std::string url::get_host() const
+{
+    return "";
+}
+
+unsigned short url::get_port() const
+{
+    return 0;
+}
+
+std::string url::get_path() const
+{
+    return "";
+}
+
+std::string url::get_query() const
+{
+    return "";
+}
+
+const std::multimap<std::string, std::string> &url::get_query_parameters() const
+{
+    static std::multimap<std::string, std::string> tmp{};
+    return tmp;
+}
+
+std::string url::get_fragment() const
+{
+    return "";
+}
+
+// path+query+fragment
+std::string url::get_full_path() const
+{
+    return "";
+}
+
+void url::from_string(const std::string &s)
+{
+}
+
+bool avant::utility::operator==(const url &a, const url &b)
+{
+    return false;
+}
+
+bool avant::utility::operator!=(const url &a, const url &b)
+{
+    return false;
+}
+
+bool avant::utility::operator<(const url &a, const url &b)
+{
+    return false;
+}
+
+void url::set_secure(bool secure)
+{
+}
+
+bool url::is_ipv6() const
+{
+    return false;
+}
+
+bool url::is_secure() const
+{
+    return false;
+}
+
+std::string url::to_string() const
+{
+    return "";
+}
+url::operator std::string() const
+{
+    return this->to_string();
+}
+
+bool url::unescape_path(const std::string &in, std::string &out)
+{
+    return false;
+}
+
+std::string_view url::capture_up_to(const std::string_view right_delimiter, const std::string &error_message /*= ""*/)
+{
+    return "";
+}
+
+bool url::move_before(const std::string_view right_delimiter)
+{
+    return false;
+}
+
+bool url::exists_forward(const std::string_view right_delimiter)
+{
+    return false;
+}
+
 std::string url::decode(const std::string &str)
 {
     std::ostringstream decoded;
--- src/utility/url.h
+++ src/utility/url.h
@@ -1,10 +1,69 @@
 #pragma once
 #include <string>
+#include <string_view>
+#include <map>
 
 namespace avant::utility
 {
+    class url;
+
+    bool operator==(const url &a, const url &b);
+    bool operator!=(const url &a, const url &b);
+    bool operator<(const url &a, const url &b);
+
     class url
     {
+    public:
+        url();
+        url(const std::string &s);
+
+        std::string get_scheme() const;
+        std::string get_username() const;
+        std::string get_password() const;
+        std::string get_host() const;
+        unsigned short get_port() const;
+        std::string get_path() const;
+        std::string get_query() const;
+        const std::multimap<std::string, std::string> &get_query_parameters() const;
+        std::string get_fragment() const;
+        std::string get_full_path() const; // path+query+fragment
+        void from_string(const std::string &s);
+        friend bool operator==(const url &a, const url &b);
+        friend bool operator!=(const url &a, const url &b);
+        friend bool operator<(const url &a, const url &b);
+        void set_secure(bool secure);
+        bool is_ipv6() const;
+        bool is_secure() const;
+        std::string to_string() const;
+        explicit operator std::string() const;
+
+    protected:
+        static bool unescape_path(const std::string &in, std::string &out);
+        std::string_view capture_up_to(const std::string_view right_delimiter, const std::string &error_message = "");
+        bool move_before(const std::string_view right_delimiter);
+        bool exists_forward(const std::string_view right_delimiter);
+
+    protected:
+        std::string scheme;
+        std::string authority;
+        std::string user_info;
+        std::string password;
+        std::string host;
+        std::string port;
+        std::string path;
+        std::string query;
+        std::multimap<std::string, std::string> query_parameters;
+        std::string fragment;
+
+        bool secure{false};
+        bool ipv6_host{false};
+        bool authority_present = false;
+
+        std::string whole_url_storage;
+        size_t left_position{0};
+        size_t right_position{0};
+        std::string_view parse_target;
+
     public:
         static std::string decode(const std::string &str);
         static std::string encode(const std::string &str);
