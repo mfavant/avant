7a1476cdeac392f9be169d96ad13bf609c6a0f9b
gaowanlu <2209120827@qq.com>
2026-06-13 15:20:56 +0800

    update: support lua hotreload with -SIGUSR1 on MacOS Apple Silicon

--- src/hooks/reload.cpp
+++ src/hooks/reload.cpp
@@ -15,14 +15,24 @@ void reload::on_cmd_reload(avant::server::server &server_obj)
         switch (server_obj.get_task_type())
         {
         case task::task_type::HTTP_TASK:
+        {
+
+            LOG_ERROR("signal on_cmd_reload http_task");
             app::http_app::on_cmd_reload(server_obj);
             break;
+        }
         case task::task_type::STREAM_TASK:
+        {
+            LOG_ERROR("signal on_cmd_reload stream_task");
             app::stream_app::on_cmd_reload(server_obj);
             break;
+        }
         case task::task_type::WEBSOCKET_TASK:
+        {
+            LOG_ERROR("signal on_cmd_reload websocket_task");
             app::websocket_app::on_cmd_reload(server_obj);
             break;
+        }
         default:
             break;
         }
